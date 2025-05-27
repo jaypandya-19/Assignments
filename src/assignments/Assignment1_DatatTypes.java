651e043f338a69cc445eaa690ec5fc361582f906
Jay Pandya <jpindia.jobs@gmail.com>
2025-05-27 20:28:53 +0530

    Assignment

---
+++ src/assignments/Assignment1_DatatTypes.java
@@ -0,0 +1,69 @@
+package assignments;
+
+public class Assignment1_DatatTypes {
+
+	public static void main(String[] args) {
+		
+		//1. Temperature of a city in degrees Celsius: 25.5
+		float temperature = 25.5f;
+		System.out.println("1. Temperature of a city in degrees Celsius: " + temperature);
+		
+		//2. Whether a customer has placed an order: true or false
+		boolean orderPlaced = true;
+		System.out.println("2. Whether a customer has placed an order: " +orderPlaced);
+		
+//		boolean orderNotPlaced = false;
+//		System.out.println("3. Whether a customer has placed an order: " +orderNotPlaced);
+		
+		//3. Person's phone number: "123-456-7890"
+		String phonenumber = "123-456-7890";
+		System.out.println("3. Person's phone number: "  + phonenumber);
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		float amount = 1000.50f;
+		System.out.println("4. Amount of money in a customer's bank account: "+ amount);
+		
+		//5. Person's email address: "john.doe@example.com"
+		String emailAddress = "john.doe@example.com";
+		System.out.println("5. Person's email address: " + emailAddress);
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		
+		//7. Person's marital status: true or false
+		boolean material = true;
+		System.out.println("7. Person's marital status:: " +material);
+		
+		//8. Person's occupation: "Software Engineer"
+		String occupation = "Software Engineer";
+		System.out.println("8. Person's occupation: " + occupation);
+		
+		//9. Person's favourite colour: "Blue"
+		String colour = "Blue";
+		System.out.println("9. Person's favourite colour: " + colour);
+		
+		//10.Current year: 2023
+		int year = 2023;
+		System.out.println("10.Current year: " + year);
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		String follower = "1,000,000";
+		System.out.println("11.Number of followers on a social media platform: " + follower );
+		
+		//12.Rating of a movie: 7.51
+		float rating =7.51f;
+		System.out.println("12.Rating of a movie: "+ rating);
+		//13.Person's blood type: 'A'
+		//14.Title of a book: "To Kill a Mockingbird"
+		//15.Number of employees in a company: 500
+		//16.Time of an event: 2:30 PM
+		//17.Name of a country: "United States"
+		//18.Person's eye color: "Brown"
+		//19.Person's birthplace: "New York City"
+		//20.Distance between two cities: 200.5
+		
+		
+		
+
+	}
+
+}
